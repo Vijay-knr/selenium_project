13f713c8c2361055a36a9774badb80945f99464c
Vijay Kayyam <vijay.kayyam@postoffice.co.uk>
2019-09-23 11:50:44 +0100

    fixing issue

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,5 +1,6 @@
 import java.net.MalformedURLException;
 import java.net.URL;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
@@ -17,13 +18,26 @@ public class GoogleTest {
     }
 
     private void browser(DesiredCapabilities dc) throws MalformedURLException, InterruptedException {
-
         dc.setPlatform(Platform.LINUX);
-        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dc);
+
+        System.out.println("*****************************************************************");
+        System.out.println(System.getenv());
+        System.out.println("*****************************************************************");
+
+//        String url = "selenium-hub";
+        String url = "http://172.19.0.2:4444";
+//        String url = "http://127.0.0.0:4444";
+//        String url = System.getenv("HUB");
+
+        url = url + "/wd/hub";
+
+        driver = new RemoteWebDriver(new URL(url), dc);
         driver.get("http://google.co.uk");
 //        driver.get("https://int.branchhub.co.uk");
         Thread.sleep(2000);
         System.out.println("Current URL is: " + driver.getCurrentUrl());
         System.out.println("Current title is: " + driver.getTitle());
+        Assert.assertTrue(driver.getCurrentUrl().contains("google"));
+        Assert.assertEquals("Google", driver.getTitle());
     }
 }
